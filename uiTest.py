a523938d39d55925792520ad75b7cba6e7397831
cykei <kelle111@naver.com>
2020-12-13 07:33:52 +0900

    상태바 추가

--- uiTest.py
+++ uiTest.py
@@ -1,12 +1,13 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QMainWindow
 from PyQt5.QtCore import QCoreApplication
-class MyApp(QWidget):
+class MyApp(QMainWindow):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
+        self.statusBar().showMessage('Ready')
 
         btn = QPushButton('Quit',self) #(버튼에 표시될 텍스트, 버튼이 위치할 부모 위젯)
         btn.resize(btn.sizeHint())
