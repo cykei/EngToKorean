6365298eb0c5572bbb0d74ea579be6da28d25055
cykei <kelle111@naver.com>
2020-12-14 00:09:00 +0900

    코드정리

--- uiTest.py
+++ uiTest.py
@@ -12,14 +12,14 @@ class MyApp(QMainWindow):
     def initUI(self):
         ## 메뉴바
         menubar = self.menuBar()
-        menubar.setNativeMenuBar(False) # mac os 에서도 동일하게 나온다.
+        menubar.setNativeMenuBar(False)
 
         exitAction = QAction('Exit', self)
         exitAction.setShortcut('Ctrl+Q')
         exitAction.setStatusTip('Exit application')
         exitAction.triggered.connect(qApp.quit)
 
-        filemenu = menubar.addMenu('&File') # Alt + F 가 File 메뉴의 단축키가 된다.
+        filemenu = menubar.addMenu('&File')
         filemenu.addAction(exitAction)
 
         aboutAction = QAction('About', self)
@@ -30,12 +30,6 @@ class MyApp(QMainWindow):
         ## 상태바
         self.statusBar().showMessage('Ready')
 
-        ## 버튼
-        # btn = QPushButton('Quit',self) #(버튼에 표시될 텍스트, 버튼이 위치할 부모 위젯)
-        # btn.resize(btn.sizeHint())
-        # btn.move(50,50)
-        # btn.clicked.connect(qApp.quit)
-
         ## 전체 창
         self.setWindowTitle("EngToKorean")
         self.resize(700,250)
@@ -43,24 +37,12 @@ class MyApp(QMainWindow):
         self.show()
 
     def center(self):
-        qr = self.frameGeometry() #창의 위치와 크기정보를 가져온다.
-        cp = QDesktopWidget().availableGeometry().center() #센터위치 파악
-        qr.moveCenter(cp) #창의 프레임을 센터 위치로 이동
-        self.move(qr.topLeft()) # 현재 창 전부를 프레임이 있는 곳으로 이동
+        qr = self.frameGeometry()
+        cp = QDesktopWidget().availableGeometry().center()
+        qr.moveCenter(cp)
+        self.move(qr.topLeft())
 
-    def body_gridlayout(self): # gridlayout 연습
-        widget = QWidget(self)
-        grid = QGridLayout(widget)
-
-        grid.addWidget(QLabel('Text:'), 0, 0)
-        grid.addWidget(QLineEdit(), 0, 1)
-
-        grid.addWidget(QPushButton('paste'),1,2)
-        grid.addWidget(QPushButton('reset'),1,3)
-
-        self.setCentralWidget(widget) # QMainWindow랑 같이 쓰려면 꼭 이 함수 써줘야 함.
-
-    def body_boxlayout(self): # 오 이거 예쁘다. 최종선택은 이걸로
+    def body_boxlayout(self):
         hbox_input = QHBoxLayout()
         hbox_input.addWidget(QLabel('Text:'))
         hbox_input.addWidget(QLineEdit())
