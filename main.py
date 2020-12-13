7dcc4db590b97fec6dc3494849a3245ee9e179f5
cykei <kelle111@naver.com>
2020-12-14 02:49:51 +0900

    초성, 중성, 종성을 조합해서 한글자 만들기 완성

--- main.py
+++ main.py
@@ -1,33 +1,57 @@
-# This is a sample Python script.
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+_CHOSUNG = [
+      'ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ',
+      'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ',
+      'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ',
+      'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ']
 
+_JUNGSUNG = [
+      'ㅏ', 'ㅐ', 'ㅑ', 'ㅒ', 'ㅓ',
+      'ㅔ', 'ㅕ', 'ㅖ', 'ㅗ', 'ㅘ',
+      'ㅙ', 'ㅚ', 'ㅛ', 'ㅜ', 'ㅝ',
+      'ㅞ', 'ㅟ', 'ㅠ', 'ㅡ', 'ㅢ',
+      'ㅣ']
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-def print_unicode(hexa):
-    print(chr(hexa))
+_JONGSUNG = [
+      '', 'ㄱ', 'ㄲ', 'ㄳ', 'ㄴ',
+      'ㄵ', 'ㄶ', 'ㄷ', 'ㄹ', 'ㄺ',
+      'ㄻ', 'ㄼ', 'ㄽ', 'ㄾ', 'ㄿ',
+      'ㅀ', 'ㅁ', 'ㅂ', 'ㅄ', 'ㅅ',
+      'ㅆ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ',
+      'ㅌ', 'ㅍ', 'ㅎ']
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    hangulNumber = (ord(u"ㄱ")*21+ord(u"ㅏ"))*28
+def printUnicode(decimal):
+    hangulNumber = decimal
     hexNumberStr = hex(hangulNumber)
     hexNumber = int(hexNumberStr, 16)
-    print(hexNumberStr)
-    print(chr(hexNumber))
-    # print_unicode(0x1100)
-    # print_unicode(0x11FF)
-    # print_unicode(0x3131)
-    # print_unicode(0x318E)
-    # print_unicode(0xAC00)
-    # print_unicode(0xD7AF)
-    # while True:
-    #     x = input()
-    #     if x == "Q":
-    #         break;
-    #     else :
-    #         print_unicode(int(x,16))
+    print(hexNumber, hexNumberStr, chr(hexNumber))
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+def mapping(target, source):
+    count = 0
+    for i in source:
+        target[i] = count
+        count += 1
+
+def printCombinedLetter(chosung,jungsung,jongsung):
+    a = CHOSUNG[chosung]
+    b = JUNGSUNG[jungsung]
+    c = JONGSUNG[jongsung]
+
+    result = (a*21 + b)*28+c
+    result = 0xAC00 + result
+    printUnicode(result)
+
+if __name__ == '__main__':
+    CHOSUNG = dict()
+    JUNGSUNG = dict()
+    JONGSUNG = dict()
+    mapping(CHOSUNG,_CHOSUNG)
+    mapping(JUNGSUNG,_JUNGSUNG)
+    mapping(JONGSUNG,_JONGSUNG)
+    print(CHOSUNG['ㄷ'])
+
+    printUnicode(ord(u'ㄱ'))
+    printUnicode(ord(u'ㅏ'))
+    printUnicode(ord(u'각'))
+
+    printCombinedLetter('ㅅ','ㅗ','ㅇ')
